71122a796dd49b5b4a53117dae234fe21de0966c
Zelarsoft <vickysaivittal@gmail.com>
2024-01-16 14:43:10 +0530

    changed names

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,18 +2,15 @@ import Favicon from "/public/images/favicon.png";
 import ClientRootLayout from "../components/ClientRootLayout";
 
 export const metadata = {
-  title: "Vaheed Shaik | Software Engineer",
+  title: "Sai Vittal | Software Engineer",
   description:
-  "Welcome to Vaheed's Software Engineering Portfolio. Explore a diverse range of projects and see how I can bring your ideas to life. Let's connect and discuss your next venture!",
+  "Welcome to Sai Vittal's Software Engineering Portfolio. Explore a diverse range of projects and see how I can bring your ideas to life. Let's connect and discuss your next venture!",
   keywords: [
     "portfolio",
     "app",
     "next.js 13",
-    "sass",
-    "scss",
     "react",
     "chakra",
-    "hashnode",
     "best portfolio",
     "best portfolio website"
   ],
